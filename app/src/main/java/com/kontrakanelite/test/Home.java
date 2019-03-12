7f831486e48142b4a399017d288be5a07345dc66
Faiz Fanani <fanani_pandemonium@yahoo.co.id>
2019-03-12 19:53:42 +0700

    initial commit

--- app/src/main/java/com/kontrakanelite/test/Home.java
+++ app/src/main/java/com/kontrakanelite/test/Home.java
@@ -2,12 +2,17 @@ package com.kontrakanelite.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Home extends AppCompatActivity {
-
+    private EditText userEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        userEmail = findViewById(R.id.etUserEmail);
+
+        String getUserEmail = Login.getEmail;
+        userEmail.setText(getUserEmail);
     }
 }
